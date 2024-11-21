7ab7889c18921cccaf0fcfec40233cdb9f2559c1
ElisBushaj <elisbushaj2@gmail.com>
2024-11-21 01:44:05 +0100

    add endpoint to retrieve list of cars with details example

--- src/routes/v1/public/index.js
+++ src/routes/v1/public/index.js
@@ -6,4 +6,43 @@ router.get("/", (req, res) => {
 	res.send("Hello from public route");
 });
 
+router.get("/cars", (req, res) => {
+	const cars = [
+		{
+			name: "1999 Toyota Corolla",
+			type: "Sedan",
+			photos: [
+				"https://t4.ftcdn.net/jpg/05/37/32/57/360_F_537325726_GtgjRiyc37BLPn9OmisBVVZec9frLaL0.jpg",
+			],
+			price: "$20,000",
+		},
+		{
+			name: "Toyota Corolla",
+			type: "Sedan",
+			photos: [
+				"https://t4.ftcdn.net/jpg/05/37/32/57/360_F_537325726_GtgjRiyc37BLPn9OmisBVVZec9frLaL0.jpg",
+			],
+			price: "$20,000",
+		},
+		{
+			name: "BMW 3 Series",
+			type: "Sedan",
+			photos: [
+				"https://t4.ftcdn.net/jpg/05/37/32/57/360_F_537325726_GtgjRiyc37BLPn9OmisBVVZec9frLaL0.jpg",
+			],
+			price: "$20,000",
+		},
+		{
+			name: "Mercedes Benz C-Class",
+			type: "Sedan",
+			photos: [
+				"https://t4.ftcdn.net/jpg/05/37/32/57/360_F_537325726_GtgjRiyc37BLPn9OmisBVVZec9frLaL0.jpg",
+			],
+			price: "$10,000",
+		},
+	];
+
+	res.status(200).json(cars);
+});
+
 export default router;
