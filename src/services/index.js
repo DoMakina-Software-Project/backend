eb3e3d9f65a78c94febcde37ffddce87832cb0f9
Erjon001 <erjonrrapi429@gmail.com>
2025-01-25 20:37:31 +0100

    add promotion price service and controller: implement CRUD operations for promotion prices and update config for cookies secret

--- src/services/index.js
+++ src/services/index.js
@@ -3,3 +3,4 @@ export { default as PassportService } from "./passport.service.js";
 export { default as AuthService } from "./auth.service.js";
 export { default as RoleService } from "./role.service.js";
 export { default as UserRoleService } from "./user-role.service.js";
+export { default as PromotionPriceService } from "./promotion-price.service.js";
---
+++ src/services/promotion-price.service.js
@@ -0,0 +1,67 @@
+import { PromotionPriceModel } from "../models/index.js";
+
+const PromotionPriceService = {
+  getPromotionPriceByPromotionId: async (promotionId) => {
+    try {
+      const promotionPrice = await PromotionPriceModel.findByPk(promotionId);
+      return promotionPrice ? promotionPrice.toJSON() : null;
+    } catch (error) {
+      console.log(
+        `PromotionPriceService.getPromotionPriceByPromotionId() error: ${error}`
+      );
+      throw error;
+    }
+  },
+
+  createPromotionPrice: async ({ promotionId, price }) => {
+    try {
+      const existingPromotionPrice =
+        await PromotionPriceService.getPromotionPriceByPromotionId(promotionId);
+      if (existingPromotionPrice)
+        throw new Error("Promotion price already exists");
+
+      const promotionPrice = await PromotionPriceModel.create({
+        promotionId,
+        price,
+      });
+      return promotionPrice ? promotionPrice.toJSON() : null;
+    } catch (error) {
+      console.log(
+        `PromotionPriceService.createPromotionPrice() error: ${error}`
+      );
+      throw error;
+    }
+  },
+  updatePromotionPrice: async ({ promotionId, price }) => {
+    try {
+      const promotionPrice = await PromotionPriceModel.findByPk(promotionId);
+      if (!promotionPrice) throw new Error("Promotion price not found");
+
+      promotionPrice.price = price;
+      await promotionPrice.save();
+      return promotionPrice.toJSON();
+    } catch (error) {
+      console.log(
+        `PromotionPriceService.updatePromotionPrice() error: ${error}`
+      );
+      throw error;
+    }
+  },
+
+  deletePromotionPrice: async (promotionId) => {
+    try {
+      const promotionPrice = await PromotionPriceModel.findByPk(promotionId);
+      if (!promotionPrice) throw new Error("Promotion price not found");
+
+      await promotionPrice.destroy();
+      return promotionPrice.toJSON();
+    } catch (error) {
+      console.log(
+        `PromotionPriceService.deletePromotionPrice() error: ${error}`
+      );
+      throw error;
+    }
+  },
+};
+
+export default PromotionPriceService;
