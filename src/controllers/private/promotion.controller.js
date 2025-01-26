7776f4866bd71d95a2954b8a0719e47be61b7ea0
Erjon001 <erjonrrapi429@gmail.com>
2025-01-26 23:54:07 +0100

    refactor brand and promotion controllers: standardize parameter naming, enhance error handling, and add create methods

--- src/controllers/private/promotion.controller.js
+++ src/controllers/private/promotion.controller.js
@@ -1,67 +1,79 @@
 import { PromotionService } from "../../services/index.js";
 export default {
-  getPromotionById: async (req, res) => {
-    try {
-      const promotionId = req.params.promotionId;
-      const promotion = await PromotionService.getPromotionById(promotionId);
+	getPromotionById: async (req, res) => {
+		try {
+			const promotionId = req.params.id;
+			const promotion = await PromotionService.getPromotionById(
+				promotionId
+			);
+			if (!promotion) {
+				return res.status(404).json({ message: "Promotion not found" });
+			}
+			return res.status(200).json(promotion);
+		} catch (error) {
+			console.log(`getPromotionById() error: ${error}`);
+			return res.status(500).json({ message: error.message });
+		}
+	},
+	createPromotion: async (req, res) => {
+		try {
+			const { brandId, name, description, startDate, endDate } = req.body;
+			const promotion = await PromotionService.createPromotion({
+				brandId,
+				name,
+				description,
+				startDate,
+				endDate,
+			});
+			if (!promotion)
+				return res
+					.status(400)
+					.json({ message: "Failed to create promotion" });
 
-      return res.status(200).json(promotion);
-    } catch (error) {
-      console.log(`getPromotionById() error: ${error}`);
-      return res.status(500).json({ message: error.message });
-    }
-  },
-  createPromotion: async (req, res) => {
-    try {
-      const { brandId, name, description, startDate, endDate } = req.body;
-      const promotion = await PromotionService.createPromotion({
-        brandId,
-        name,
-        description,
-        startDate,
-        endDate,
-      });
-      if (!promotion)
-        return res.status(400).json({ message: "Failed to create promotion" });
+			return res.status(201).json(promotion);
+		} catch (error) {
+			console.log(`createPromotion() error: ${error}`);
+			return res.status(500).json({ message: "Internal server error" });
+		}
+	},
+	updatePromotion: async (req, res) => {
+		try {
+			const promotionId = req.params.id;
+			const { brandId, name, description, startDate, endDate } = req.body;
+			const promotion = await PromotionService.updatePromotion({
+				promotionId,
+				brandId,
+				name,
+				description,
+				startDate,
+				endDate,
+			});
+			if (!promotion)
+				return res
+					.status(404)
+					.json({ message: "Failed to update promotion" });
 
-      return res.status(201).json(promotion);
-    } catch (error) {
-      console.log(`createPromotion() error: ${error}`);
-      return res.status(500).json({ message: "Internal server error" });
-    }
-  },
-  updatePromotion: async (req, res) => {
-    try {
-      const promotionId = req.params.promotionId;
-      const { brandId, name, description, startDate, endDate } = req.body;
-      const promotion = await PromotionService.updatePromotion({
-        promotionId,
-        brandId,
-        name,
-        description,
-        startDate,
-        endDate,
-      });
-      if (!promotion)
-        return res.status(404).json({ message: "Failed to update promotion" });
+			return res.status(200).json(promotion);
+		} catch (error) {
+			console.log(`updatePromotion() error: ${error}`);
+			return res.status(500).json({ message: error.message });
+		}
+	},
+	deletePromotion: async (req, res) => {
+		try {
+			const promotionId = req.params.id;
+			const promotion = await PromotionService.deletePromotion(
+				promotionId
+			);
+			if (!promotion)
+				return res
+					.status(404)
+					.json({ message: "Failed to delete promotion" });
 
-      return res.status(200).json(promotion);
-    } catch (error) {
-      console.log(`updatePromotion() error: ${error}`);
-      return res.status(500).json({ message: error.message });
-    }
-  },
-  deletePromotion: async (req, res) => {
-    try {
-      const promotionId = req.params.promotionId;
-      const promotion = await PromotionService.deletePromotion(promotionId);
-      if (!promotion)
-        return res.status(404).json({ message: "Failed to delete promotion" });
-
-      return res.status(200).json(promotion);
-    } catch (error) {
-      console.log(`deletePromotion() error: ${error}`);
-      return res.status(500).json({ message: error.message });
-    }
-  },
+			return res.status(200).json(promotion);
+		} catch (error) {
+			console.log(`deletePromotion() error: ${error}`);
+			return res.status(500).json({ message: error.message });
+		}
+	},
 };
